cfa0f59d3cf66ae96ae064ca4cac2a74a73bc9d7
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-04-05 16:34:45 +0700

    remove the actions

--- .template/.php-cs-fixer.php
+++ .template/.php-cs-fixer.php
@@ -241,7 +241,7 @@ $finder = Finder::create()
         __DIR__.'/app',
         __DIR__.'/config',
         __DIR__.'/database',
-        __DIR__.'/lang',
+        __DIR__.'/resources/lang',
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
